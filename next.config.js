166f8cd8c02459988ca0abfd3e96e43c6c95c434
허제인 <codingjamee@gmail.com>
2025-01-14 10:04:12 +0900

    next config 파일 추가

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  styledComponents: true,
+};
+
+module.exports = nextConfig;
